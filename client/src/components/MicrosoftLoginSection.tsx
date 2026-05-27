5cf7863943b3850f0b96505eb460a8c34c888c45
Lukas Neubert <40118727+serkonda7@users.noreply.github.com>
2026-05-27 13:24:37 +0200

    back to css

--- client/src/components/MicrosoftLoginSection.tsx
+++ client/src/components/MicrosoftLoginSection.tsx
@@ -1,6 +1,6 @@
 import type { Component, JSX } from 'solid-js'
 import msLogo from '../img/ms-symbollockup_mssymbol_19.svg'
-import '../css/microsoft-login.scss'
+import '../css/microsoft-login.css'
 
 type Props = {
 	localLoginForm: JSX.Element
--- client/src/global.d.ts
+++ client/src/global.d.ts
@@ -1,5 +1,4 @@
 declare module '*.css'
-declare module '*.scss'
 declare module '*.svg' {
 	const src: string
 	export default src
