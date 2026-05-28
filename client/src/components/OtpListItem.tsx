459d4e65d39546ca48ccb7a4042ae70615ad345d
Lukas Neubert <40118727+serkonda7@users.noreply.github.com>
2026-05-28 21:37:11 +0200

    add code placeholders

--- client/src/components/OtpListItem.tsx
+++ client/src/components/OtpListItem.tsx
@@ -16,8 +16,13 @@ const OtpListItem: Component<Props> = (props) => {
 	return (
 		<li class="otp-list__item">
 			<button type="button" class="otp-list__entry" onClick={props.onClick}>
-				<span class="otp-list__issuer">{issuerText}</span>
-				<span class="otp-list__label">{props.otp.label}</span>
+				<span class="otp-list__text">
+					<span class="otp-list__issuer">{issuerText}</span>
+					<span class="otp-list__label">{props.otp.label}</span>
+				</span>
+				<span class="otp-list__code-placeholder" aria-hidden="true">
+					&ast; &ast; &ast; &ast; &ast; &ast;
+				</span>
 			</button>
 		</li>
 	)
