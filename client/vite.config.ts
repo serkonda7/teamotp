ebedeff409a02c0a8e9afd86f95ea5e331825bbc
Lukas Neubert <40118727+serkonda7@users.noreply.github.com>
2026-05-28 23:22:30 +0200

    mimic tree shaking in development

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,27 @@
+import { resolve } from 'node:path'
 import devtools from 'solid-devtools/vite'
 import { defineConfig } from 'vite'
+import entryShakingPlugin from 'vite-plugin-entry-shaking'
 import solidPlugin from 'vite-plugin-solid'
 
+const tablerIconsEntry = resolve(
+	import.meta.dirname,
+	'node_modules/@tabler/icons-solidjs/dist/source/icons/index.js',
+)
+
 export default defineConfig({
-	plugins: [devtools(), solidPlugin()],
+	plugins: [
+		entryShakingPlugin({
+			targets: [tablerIconsEntry],
+		}),
+		devtools(),
+		solidPlugin(),
+	],
+	resolve: {
+		alias: {
+			'@tabler/icons-solidjs': tablerIconsEntry,
+		},
+	},
 	server: {
 		port: 5371,
 		proxy: {
