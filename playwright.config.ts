80913a6624aa8821a6b8620b008bf6a7869cb1b5
Lukas Neubert <40118727+serkonda7@users.noreply.github.com>
2026-06-01 12:12:21 +0200

    ci: playwright log upload

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
 	globalSetup: './tests/e2e/global-setup.ts',
 	fullyParallel: true,
 	forbidOnly: !!process.env.CI,
-	retries: process.env.CI ? 2 : 0,
+	retries: process.env.CI ? 1 : 0,
 	workers: process.env.CI ? 1 : undefined,
 	reporter: [['html', { open: 'never' }], ['list']],
 	use: {
