ec63236ada7a7280ea5fa40b3053660b335b0825
Lukas Neubert <40118727+serkonda7@users.noreply.github.com>
2026-06-17 17:42:07 +0200

    remove unused code from TeamOtpLogo

--- client/src/components/TeamOtpLogo.tsx
+++ client/src/components/TeamOtpLogo.tsx
@@ -3,22 +3,12 @@ import teamOtpIcon from '../img/teamotp_icon.svg'
 
 type TeamOtpLogoProps = {
 	class?: string
-	textClass?: string
-	iconClass?: string
-	text?: string
 }
 
 const TeamOtpLogo: Component<TeamOtpLogoProps> = (props) => (
 	<span class={`teamotp-logo ${props.class ?? ''}`.trim()}>
-		<img
-			class={`teamotp-logo__icon ${props.iconClass ?? ''}`.trim()}
-			src={teamOtpIcon}
-			alt=""
-			aria-hidden="true"
-		/>
-		<span class={`teamotp-logo__text ${props.textClass ?? ''}`.trim()}>
-			{props.text ?? 'TeamOTP'}
-		</span>
+		<img class="teamotp-logo__icon" src={teamOtpIcon} alt="" aria-hidden="true" />
+		<span class="teamotp-logo__text">TeamOTP'</span>
 	</span>
 )
 
