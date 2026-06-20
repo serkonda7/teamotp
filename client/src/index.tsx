f782537cae5f0cfa7ef78d83661e82cd6513b163
Lukas Neubert <40118727+serkonda7@users.noreply.github.com>
2026-06-20 12:13:52 +0200

    lint: useDomQuerySelector

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,7 @@ import { render } from 'solid-js/web'
 import App from './App'
 import './css/styles.css'
 
-const root = document.getElementById('root')
+const root = document.querySelector('#root')
 
 // biome-ignore lint/style/noNonNullAssertion: we know it exists
 render(() => <App />, root!)
