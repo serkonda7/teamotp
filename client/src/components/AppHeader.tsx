a360476716d411190952b0485ea723ec0247b582
Lukas Neubert <40118727+serkonda7@users.noreply.github.com>
2026-07-13 14:12:57 +0200

    select input on refocus

--- client/src/components/AppHeader.tsx
+++ client/src/components/AppHeader.tsx
@@ -67,7 +67,10 @@ const AppHeader = (props: AppHeaderProps): JSX.Element => {
 					onInput={(event: InputEventAndTarget): void => {
 						props.onSearchInput(event.currentTarget.value)
 					}}
-					onFocus={() => setIsSearchFocused(true)}
+					onFocus={() => {
+						setIsSearchFocused(true)
+						searchInputRef?.select()
+					}}
 					onBlur={() => setIsSearchFocused(false)}
 					placeholder="Suche"
 					aria-label="OTP-Einträge suchen"
